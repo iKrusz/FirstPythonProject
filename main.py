3215cb050c6519c8ebc980f002a1dae34c2f3674
iKrusz <bartosz@247studio.co>
2021-11-07 21:10:09 +0100

    Working program

--- main.py
+++ main.py
@@ -12,9 +12,7 @@ możesz wykorzystać też klasy
 
 
 class Encryptor:
-    def __init__(self, input_string_):
-        self.input_str = input_string_
-        self.encrypted_str = ""
+    def __init__(self):
         # rot_dict = {'a': 'n', 'b': 'o', 'c': 'p',
         #             'd': 'q', 'e': 'r', 'f': 's',
         #             'g': 't', 'h': 'u', 'i': 'v',
@@ -24,7 +22,7 @@ class Encryptor:
         #             's': 'f', 't': 'g', 'u': 'h',
         #             'v': 'i', 'w': 'j', 'x': 'k',
         #             'y': 'l', 'z': 'm'}
-        rot_dict_pl = {'a': 'k', 'ą': 'l', 'b': 'ł',
+        self.rot_dict_pl = {'a': 'k', 'ą': 'l', 'b': 'ł',
                        'c': 'm', 'ć': 'n', 'd': 'ń',
                        'e': 'o', 'ę': 'ó', 'f': 'p',
                        'g': 'q', 'h': 'r', 'i': 's',
@@ -37,20 +35,60 @@ class Encryptor:
                        'x': 'f', 'y': 'g', 'z': 'h',
                        'ź': 'i', 'ż': 'j'}
 
+    def encypt_and_save(self, input_string_):
+        self.input_str = input_string_
+        self.encrypted_str = ""
         for letter in self.input_str:
             if letter.islower():
-                self.encrypted_str += rot_dict_pl.get(letter)
+                self.encrypted_str += self.rot_dict_pl.get(letter)
             if letter.isupper():
                  letter = letter.lower()
-                 self.encrypted_str += rot_dict_pl.get(letter).capitalize()
-            if letter not in rot_dict_pl:
+                 self.encrypted_str += self.rot_dict_pl.get(letter).capitalize()
+            if letter not in self.rot_dict_pl:
                 self.encrypted_str += letter
 
         with open("encrypted.txt", "a", encoding="utf-8") as file:
             file.writelines(f"{self.encrypted_str}\n")
+        print("Zaszyfrowana fraza została zapisana w pliku")
 
-Encryptor("Siemanko")
-Encryptor("tutaj")
-Encryptor("program")
-Encryptor("szyfrujący")
+    def read_and_decrypt(self, line_nr):
+        with open("encrypted.txt", "r", encoding="utf-8") as file:
+            self.out_line = []
+            self.lines = file.readlines()
+            for line in self.lines:
+                self.out_line.append(line.rstrip())
 
+        inv_dict = {v: k for k, v in self.rot_dict_pl.items()}
+        self.decrypted_str = ""
+        for letter in self.out_line[line_nr]:
+            if letter.islower():
+                self.decrypted_str += inv_dict.get(letter)
+            if letter.isupper():
+                letter = letter.lower()
+                self.decrypted_str += inv_dict.get(letter).capitalize()
+            if letter not in inv_dict:
+                self.decrypted_str += letter
+        print(self.decrypted_str)
+
+def menu():
+    print("\n\nCo chcesz zrobić? \n")
+    print("1. Zaszyfruj frazę")
+    print("2. Odszyfruj frazę")
+    print("3. Zakończ program")
+    choice = input("\nWybierz akcję: ")
+    encryptor = Encryptor()
+    if choice == "1":
+        encryptor.encypt_and_save(input("Wprowadź frazę do zaszyfrowania: "))
+    elif choice == "2":
+        encryptor.read_and_decrypt(int(input("Którą linię chcesz odczytać? ")))
+    elif choice == "3":
+        exit()
+    else:
+        print("Nieprawidłowy wybór!")
+
+def main():
+    menu()
+
+if __name__ == "__main__":
+    while True:
+        main()
